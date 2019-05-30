76da7dfef5b1da727a705491fe0b0df4f9cfa230
Agustina Boujon <agus.boujon10@gmail.com>
2019-05-30 10:04:32 -0300

    test

--- src/main/java/bunnytail/bunnytail/entity/Home.java
+++ src/main/java/bunnytail/bunnytail/entity/Home.java
@@ -1,6 +1,13 @@
 package bunnytail.bunnytail.entity;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class Home extends Canvas {
+    private static final long serialVersionId = 1L;
+    private static final int HEIGHT = 800;
+    private static final int WIDTH = 800;
+    private static JFrame window;
+
+    private Home(){}
 }
